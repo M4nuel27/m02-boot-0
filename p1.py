8665459160671971bb2a69b64fc943b5272ec984
M4nuel27 <56842520+M4nuel27@users.noreply.github.com>
2022-02-21 20:20:53 +0100

    Add files via upload

--- p1.py
+++ p1.py
@@ -6,3 +6,14 @@ def sumaTodos(limitTo):
     return resultado
     
 print(sumaTodos(100))
+
+
+def sumaTodoslosCuadrados(limitTo):
+    resultado = 0
+    for i in range(0, limitTo+1):
+        resultado += i*i
+        
+    return resultado
+    
+print(sumaTodos(100))
+print(sumaTodosolosCuadrados(3))
